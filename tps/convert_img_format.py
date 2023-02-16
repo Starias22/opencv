1ac109fbcc4da85e24697e0d641180bb1587e276
=coderoom <adedeezechiel@gmail.com>
2023-02-16 20:36:54 +0100

    image conversion

--- tps/convert_img_format.py
+++ tps/convert_img_format.py
@@ -4,36 +4,54 @@ img_exts = ['bmp', 'jpg', 'jpeg', 'jpe', 'jp2', 'png',
                     'webp', 'pbm', 'pgm', 'ppm', 'sr', 'ras', 'tiff', 'tif']
 
 def convert(inputPath,outputPath):
-    #get the exetension of the output file
-    ext=outputPath.split('.')[-1]
+
     #img reading
     try:
         #try to read the image
         img=cv.imread(inputPath )
         """,cv.IMREAD_GRAYSCALE"""
-        if ext is( 'pgm' or 'pbm'):
-            if len(img.shape)==3:
-                print("You're using",ext,"file etension which doesnt support",
-                        'bgr while the input file is bgr','Press O/o if you',
-                        'want the output file be a brayscale image.',
-                        'Press any key else to exit')
-                key=cv.waitKey(0)
-                #if O/o not pressed
-                if key!=ord('O'):
-                    print('Converting aborted!')
-                    return
-                #else(O/ key is pressed)
-                #convert img to rayscale
-                img=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+
 
         #if failure
         if img is None:
             print('File not found or wrong input file')
             #nothing else to do
             return
+        #img=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+
         #else
-        cv.imshow('Image to convert',img)
+        #get the exetension of the output file
+        ext=outputPath.split('.')[-1]
+        #if the extension is ppm
+        if ext=='ppm':
+            #if img is not bgr
+            if len(img.shape)!=3:
+                print("Conversion impossible: You're using",ext,"file extension which doesn't support",
+                        'grayscale while the input file is grayscale.\n')
+                return
+
+        #if the extension is pgm or pbm
+        if ext =='pgm' or ext=='pbm':
+            if len(img.shape)==3:
+                print("You're using",ext,"file extension which doesnt support",
+                        'bgr while the input file is bgr.\n','Press O/o if you',
+                        'want the output file be a grayscale image.\n',
+                        'Press any key else to exit')
+                cv.imshow('Image to convert',img)
+                key=cv.waitKey(0)
+                #if O/o not pressed
+                if key!=ord('o'):
+                    #print('****key:',key)
+                    print('Converting aborted!')
+                    return
+                #else(O/ key is pressed)
+                #convert img to grayscale
+                img=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+
         print('File converting: Press a key to continue')
+        #show the image
+        cv.imshow('Image to convert',img)
+        #wait for a key to continue
         cv.waitKey()
         #assert the extension is in the exts list
         assert ext in img_exts
@@ -48,6 +66,7 @@ def convert(inputPath,outputPath):
     else:
         print('Image successfully converted to',ext)
 inputPath='../images/apple.jpeg'
+
 """convert(inputPath,outputPath='')#wrong
 convert(inputPath,outputPath='ke')#wrong
 convert(inputPath,outputPath='out.org')#wrong"""
@@ -63,7 +82,11 @@ convert(inputPath,outputPath='out.ras')#right
 convert(inputPath,outputPath='out.tiff')#right
 convert(inputPath,outputPath='out.tif')#right
 convert(inputPath,outputPath='out.sr')#right
+convert(inputPath,outputPath='out.ppm')#right # doesn't support grayscale:only color images
 
-convert(inputPath,outputPath='out.ppm')#right # doesn't support grayscale
+
 convert(inputPath,outputPath='out.pgm')#right doesn't support bgr
 convert(inputPath,outputPath='out.pbm')#right  doesn't support bgr
+
+convert('./out.sr',outputPath='out.sr')#right
+
